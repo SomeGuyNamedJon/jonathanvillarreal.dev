1cd82c1f97d0fcc9419a2088076a7bc262aa6d76
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-01-28 10:54:16 -0800

    cleaned up unnecessary code

--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -1,17 +1,9 @@
 <script>
-    import {computed} from 'vue'
-    import {useRoute} from 'vue-router'
-
     export default {
         props: {
             page: {type: String, required: true},
             icon: {type: String, required: true},
             name: {type: String, required: true},
-        },
-        setup(props){
-            const route = useRoute()
-            const isActive = computed(() => route.name === props.page)
-            return {isActive}
         }
     }
 </script>
