b9688b2f5c00bea4f9a468e59365492ee9764988
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-01-31 14:20:13 -0800

    added more projects

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -6,12 +6,27 @@
 </script>
 
 <template>
-    <RepoCard user="SomeGuyNamedJon" repo="barify"/>
-    <RepoCard user="SomeGuyNamedJon" repo="BurgerGame"/>
+    <hr>
+    <h2>Game Projects</h2>
+    <hr>
+    <RepoCard user="beastboy241" repo="hotelReservation_CS3773"/>
+    <RepoCard user="e76971072" repo="Into-the-Unlit-Game"/>
     <RepoCard user="SomeGuyNamedJon" repo="UntitledCubeRunner"/>
+    <RepoCard user="SomeGuyNamedJon" repo="UnfinishedFPS"/>
+    <RepoCard user="SomeGuyNamedJon" repo="PolygonDefender"/>
+    
+    <hr>
+    <h2>Linux Stuff</h2>
+    <hr>
+    <RepoCard user="SomeGuyNamedJon" repo="barify"/>
     <RepoCard user="SomeGuyNamedJon" repo="laptop-dotfiles"/>
+    
+    <hr>
+    <h2>Web Dev</h2>
+    <hr>
     <RepoCard user="SomeGuyNamedJon" repo="CompanyEquipment"/>
     <RepoCard user="SomeGuyNamedJon" repo="PersonalPage"/>
+    <RepoCard user="SomeGuyNamedJon" repo="martykitchenart"/>
 </template>
 
 <style scoped>
