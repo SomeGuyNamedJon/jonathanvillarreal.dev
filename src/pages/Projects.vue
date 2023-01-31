f81b4ecb2ef854ac4eb8e76fc4beec561df459b6
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-01-31 14:50:57 -0800

    reordered projects again

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -7,26 +7,24 @@
 
 <template>
     <hr>
-    <h2>Game Projects</h2>
-    <hr>
-    <RepoCard user="SomeGuyNamedJon" repo="UntitledCubeRunner"/>
-    <RepoCard user="SomeGuyNamedJon" repo="UnfinishedFPS"/>
-    <RepoCard user="SomeGuyNamedJon" repo="PolygonDefender"/>
-    <RepoCard user="e76971072" repo="Into-the-Unlit-Game"/>
-    
-    <hr>
-    <h2>Linux Stuff</h2>
-    <hr>
-    <RepoCard user="SomeGuyNamedJon" repo="barify"/>
-    <RepoCard user="SomeGuyNamedJon" repo="laptop-dotfiles"/>
-    
-    <hr>
     <h2>Web Dev</h2>
     <hr>
     <RepoCard user="SomeGuyNamedJon" repo="martykitchenart"/>
     <RepoCard user="SomeGuyNamedJon" repo="PersonalPage"/>
     <RepoCard user="SomeGuyNamedJon" repo="CompanyEquipment"/>
     <RepoCard user="beastboy241" repo="hotelReservation_CS3773"/>
+    <hr>
+    <h2>Linux Stuff</h2>
+    <hr>
+    <RepoCard user="SomeGuyNamedJon" repo="barify"/>
+    <RepoCard user="SomeGuyNamedJon" repo="laptop-dotfiles"/>
+    <hr>
+    <h2>Game Projects</h2>
+    <hr>
+    <RepoCard user="SomeGuyNamedJon" repo="UntitledCubeRunner"/>
+    <RepoCard user="SomeGuyNamedJon" repo="UnfinishedFPS"/>
+    <RepoCard user="SomeGuyNamedJon" repo="PolygonDefender"/>
+    <RepoCard user="e76971072" repo="Into-the-Unlit-Game"/>  
 </template>
 
 <style scoped>
