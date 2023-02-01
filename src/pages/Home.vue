a5ddf02ebeec3ddf535e83416752dd841f141b66
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-01-31 19:30:12 -0800

    tenative content update for home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,11 +4,14 @@
         <img src="@/assets/images/Me.jpg"/>
     </div>
     <div class="Profile">
-        <h2>Title</h2>
+        <h2>Who am I?</h2>
         <p>
-            Labore quis aliquip labore ut ut in duis do exercitation. Ipsum veniam laboris duis laboris consequat sunt ullamco minim. Sunt cillum culpa nulla quis culpa sint pariatur do id ex voluptate. Nisi magna reprehenderit incididunt pariatur ex incididunt dolore sit Lorem reprehenderit. Laboris proident veniam excepteur exercitation pariatur consectetur laboris amet qui irure id.
-            Labore mollit ex est ex fugiat mollit nostrud Lorem. Dolore cupidatat veniam sunt nostrud amet commodo. Eu ex exercitation aliqua fugiat mollit sint deserunt excepteur magna et dolor et. Ea minim cupidatat fugiat ad cupidatat elit labore nostrud deserunt qui. Cupidatat dolor consequat esse ad do magna. Occaecat labore aute aliquip adipisicing deserunt eiusmod enim quis.
-            Reprehenderit Lorem sint sint est est quis culpa ut amet adipisicing reprehenderit culpa occaecat culpa. Id aute qui ut et labore et labore. Exercitation ex Lorem ipsum consectetur quis. Voluptate nulla reprehenderit cupidatat velit ut. Irure eu et dolor dolore non consectetur laborum. Esse anim occaecat occaecat irure eu est deserunt ut magna esse deserunt Lorem non adipisicing. Minim elit adipisicing irure nostrud eu aliquip enim.
+            Hi, I'm Jon, an aspiring software engineer and recent university graduate. 
+            I'm passionate about video games, cartoons, anime, and technology. I'm a Linux and FOSS fanatic.
+            I grew up in San Antonio, TX. It's where I attained my education and spent most of my life.
+            I've recently moved to California to live with my fiancée and her family. I'm looking towards
+            joining the work force as a new developer. Always looking forward towards expanding my knowledge
+            and eager to make a name for myself.
         </p>
     </div>
     </div>
@@ -28,5 +31,7 @@
     }
     .Profile{
         grid-area: Profile;
+        text-align: justify;
+        margin-right: 20%;
     }
 </style>
