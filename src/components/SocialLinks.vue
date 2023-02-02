11ecce3ff78bfbe3255fce9bbe0d13cfa3cfeb90
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-02-02 04:22:59 -0800

    completed social links component

--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -1,3 +1,24 @@
 <template>
-    <font-awesome-icon icon="fa-brands fa-github"/>
+    <a href="https://www.github.com/SomeGuyNamedJon" target="_blank">
+        <font-awesome-icon class="link" icon="fa-brands fa-github"/>
+    </a>
+    <a href="https://www.linkedin.com/in/jonathan-villarreal-3a5a73136/" target="_blank">
+        <font-awesome-icon class="link" icon="fa-brands fa-linkedin"/>
+    </a>
+    <a href="https://twitter.com/named_jon" target="_blank">
+        <font-awesome-icon class="link" icon="fa-brands fa-twitter"/>
+    </a>
 </template>
+
+<style scoped>
+    a {
+        color: #f0f0f0
+    }
+    a:hover{
+        color: #a0a0a0;
+    }
+    .link{
+        height: 25px;
+        margin-inline: 3px;
+    }
+</style>
