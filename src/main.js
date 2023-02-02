59765ae9f0f69415d591028258bfd5d5a65c76d8
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-02-02 04:06:09 -0800

    added other free font awesome icons

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import router from '@/plugins/router'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fas } from "@fortawesome/free-solid-svg-icons"
+import { far } from "@fortawesome/free-regular-svg-icons"
+import { fab } from "@fortawesome/free-brands-svg-icons"
 
-library.add(fas)
+library.add(fas, far, fab)
 
 createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
