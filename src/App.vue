348e47dd0adf84a702c927c2c65771e98d05b9e9
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-02-01 17:46:57 -0800

    added perspective grid of dots to make things look more stylish

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <template>
  <div class="PageView">
   <div class="Title">
-    <img id="banner" src="@/assets/images/JonathanV.png" height="150"/>
+    <img id="banner" src="@/assets/images/JonathanV.png" height="100"/>
   </div>
   <NavBar/>
   <div class="RouterView">
@@ -15,8 +15,24 @@
 </template>
 
 <style>
+.PageView {
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  perspective: 1px;
+}
+
 .Title {
   background-color: var(--deep-purple);
   padding-block: 15px;
 }
+
+body{
+  background: radial-gradient(#333 2px, transparent 1px) 0 0 / 15px 15px; 
+  transform-style: preserve-3d;
+  transform: translateZ(-1px);
+  z-index: -1;
+}
+
+
 </style>
