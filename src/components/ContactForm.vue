a78ac28555da74cef8a2d1d9083d37c3a5315812
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2023-02-02 02:12:10 -0800

    style change

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -190,7 +190,7 @@
     margin: 8px 0;
     display: inline-block;
     color: #fff;
-    background-color: var(--grey);
+    background-color: var(--background);
     border: 2px solid var(--purple);
     border-radius: 4px;
     box-sizing: border-box;
